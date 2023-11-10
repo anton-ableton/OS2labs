ac6a4066b23833e29f4676837efff3647c9d2255
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-11-10 21:10:54 +0700

    Create f.c

---
+++ lab1/2/f.c
@@ -0,0 +1,39 @@
+//gcc f.c -o f -pthread
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *thread_function(void *arg) {
+  char *message = "hello world";
+  return message;
+}
+
+int main() {
+  pthread_t tid; 
+  int err;
+
+  pthread_attr_t attr;
+  err = pthread_attr_init(&attr);
+  if (err) {
+    printf("Main thread: pthread_attr_init() failed: %s\n", strerror(err));
+    return -1;
+  }
+
+  err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+  if (err) {
+    printf("Main thread: pthread_attr_setdetachstate() failed: %s\n", strerror(err));
+    return -1;
+  }
+
+  err = pthread_create(&tid, &attr, thread_function, NULL);
+  if (err) {
+    printf("Main thread: pthread_create() failed: %s\n", strerror(err));
+    return -1;
+  }
+
+  pthread_attr_destroy(&attr);
+
+
+  return 0;
+}
