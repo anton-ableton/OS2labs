5eba03200a5e850174f90f0b9e04163ae5a425e3
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-11-10 21:11:37 +0700

    Create b.c

---
+++ lab1/3/b.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct MyStruct {
+  int intValue;
+  char *stringValue;
+};
+
+void *thread_function(void *arg) {
+  struct MyStruct *data = (struct MyStruct *)arg;
+  
+  printf("Thread: intValue = %d, stringValue = %s\n", data->intValue, data->stringValue);
+
+  return NULL;
+}
+
+int main() {
+  pthread_t tid;
+  pthread_attr_t attr; 
+
+  pthread_attr_init(&attr);
+
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+  struct MyStruct *myData = (struct MyStruct *)malloc(sizeof(struct MyStruct));
+
+  if (myData == NULL) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+
+  myData->intValue = 42;
+  myData->stringValue = "Hello, Thread!";
+
+  int result = pthread_create(&tid, &attr, thread_function, myData);
+  if (result != 0) {
+    perror("pthread_create");
+    free(myData); 
+    exit(EXIT_FAILURE);
+  }
+
+  pthread_attr_destroy(&attr);
+
+  pthread_exit(NULL);
+}
