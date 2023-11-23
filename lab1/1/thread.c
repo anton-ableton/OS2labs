f1bdb4d4d3babcc0c9113fe70be68144e66884a0
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-11-23 11:57:59 +0700

    Update thread.c
    reformat

--- lab1/1/thread.c
+++ lab1/1/thread.c
@@ -1,3 +1,4 @@
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
@@ -6,28 +7,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
-void *mythread(void *arg) {
-  //info about process (PID), parent process (PPID) and stream identifier (TID)
-	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+void *mythread(void *arg)
+{
+	//info about process (PID), parent process (PPID) and stream identifier (TID)
+	printf("mythread[%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
 	return NULL;
 }
 
-int main() {
+int main()
+{
 	// the data type used to represent the identifier
 	pthread_t tid;
 	int err;
 
-	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+	printf("main[%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
 
 	// 1) id 2) attributes 3) pointer to function 4) function arg 
 	err = pthread_create(&tid, NULL, mythread, NULL);
-	if (err) {
+	if (err)
+	{
 		printf("main: pthread_create() failed: %s\n", strerror(err));
 		return -1;
 	}
 
-  sleep(30);
+	sleep(30);
 
 	return 0;
 }
