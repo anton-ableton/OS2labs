f2e8d3c94d407d958ab5759c257534fc745118d1
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-11-23 11:45:19 +0700

    Update b.c
    free in thread func

--- lab1/3/b.c
+++ lab1/3/b.c
@@ -2,45 +2,52 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-struct MyStruct {
-  int intValue;
-  char *stringValue;
+struct MyStruct
+{
+	int intValue;
+	char *stringValue;
 };
 
-void *thread_function(void *arg) {
-  struct MyStruct *data = (struct MyStruct *)arg;
-  
-  printf("Thread: intValue = %d, stringValue = %s\n", data->intValue, data->stringValue);
+void *thread_function(void *arg)
+{
+	struct MyStruct *data = (struct MyStruct *) arg;
 
-  return NULL;
+	printf("Thread: intValue = %d, stringValue = %s\n", data->intValue, data->stringValue);
+
+	free(arg)
+
+	return NULL;
 }
 
-int main() {
-  pthread_t tid;
-  pthread_attr_t attr; 
+int main()
+{
+	pthread_t tid;
+	pthread_attr_t attr;
 
-  pthread_attr_init(&attr);
+	pthread_attr_init(&attr);
 
-  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
-  struct MyStruct *myData = (struct MyStruct *)malloc(sizeof(struct MyStruct));
+	struct MyStruct *myData = (struct MyStruct *) malloc(sizeof(struct MyStruct));
 
-  if (myData == NULL) {
-    perror("malloc");
-    exit(EXIT_FAILURE);
-  }
+	if (myData == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-  myData->intValue = 42;
-  myData->stringValue = "Hello, Thread!";
+	myData->intValue = 42;
+	myData->stringValue = "Hello, Thread!";
 
-  int result = pthread_create(&tid, &attr, thread_function, myData);
-  if (result != 0) {
-    perror("pthread_create");
-    free(myData); 
-    exit(EXIT_FAILURE);
-  }
+	int result = pthread_create(&tid, &attr, thread_function, myData);
+	if (result != 0)
+	{
+		perror("pthread_create");
+		free(myData);
+		exit(EXIT_FAILURE);
+	}
 
-  pthread_attr_destroy(&attr);
+	pthread_attr_destroy(&attr);
 
-  pthread_exit(NULL);
+	pthread_exit(NULL);
 }
