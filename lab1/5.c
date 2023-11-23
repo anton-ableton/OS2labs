3338e95a51bdc43eee0ee47a657f8b2d96777f8c
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-11-23 11:56:55 +0700

    Update 5.c
    replace printf() with write() in handler funcs

--- lab1/5.c
+++ lab1/5.c
@@ -1,33 +1,35 @@
-//gcc 5.c -o 5 -std=c99 -pthread 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
+#include <unistd.h>
 
 void sigint_handler(int signo) {
-  printf("Received SIGINT in Thread 2\n"); 
+  const char* msg = "Int\n";
+  write(STDOUT_FILENO, msg, strlen(msg)); 
 }
 
 void *thread2_function(void *arg) {
-  
   signal(SIGINT, sigint_handler);
 
   while (1) {}
   return NULL;
-  
+}
+
+void sigquit_handler(int signo) {
+  const char* msg = "Quit\n";
+  write(STDOUT_FILENO, msg, strlen(msg));
 }
 
 void *thread3_function(void *arg) {
   int sig;
   sigset_t set;
 
-  // Устанавливаем маску для sigwait
   sigemptyset(&set);
   sigaddset(&set, SIGQUIT);
 
   sigwait(&set, &sig);
-  printf("Received SIGQUIT in Thread 3\n");
+  sigquit_handler(sig);
 
   return NULL;
 }
@@ -40,13 +42,13 @@ int main() {
 
   err = pthread_create(&tid2, NULL, thread2_function, NULL);
   if (err) {
-    printf("Main thread: pthread_create() for Thread 2 failed: %s\n", strerror(err));
+    printf("Main thread: pthread_create() for Thread 2 failed\n");
     return -1;
   }
 
   err = pthread_create(&tid3, NULL, thread3_function, NULL);
   if (err) {
-    printf("Main thread: pthread_create() for Thread 3 failed: %s\n", strerror(err));
+    printf("Main thread: pthread_create() for Thread 3 failed\n");
     return -1;
   }
 
@@ -56,13 +58,13 @@ int main() {
 
   err = pthread_join(tid2, NULL);
   if (err) {
-    printf("Main thread: pthread_join() for Thread 2 failed: %s\n", strerror(err));
+    printf("Main thread: pthread_join() for Thread 2 failed\n");
     return -1;
   }
 
   err = pthread_join(tid3, NULL);
   if (err) {
-    printf("Main thread: pthread_join() for Thread 3 failed: %s\n", strerror(err));
+    printf("Main thread: pthread_join() for Thread 3 failed\n");
     return -1;
   }
 
