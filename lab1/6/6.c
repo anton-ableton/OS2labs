65140a74ea154889cfc328fe2a401ea8e5f99701
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-11-25 13:53:11 +0700

    Update 6.c

--- lab1/6/6.c
+++ lab1/6/6.c
@@ -1,4 +1,4 @@
-// Compilation: gcc -o 6 6.c -lpthread
+// Compilation: gcc -o 6 6.c mythread.c -lpthread
 
 #include <stdio.h>
 #include <pthread.h>
@@ -11,7 +11,7 @@ void* mythread(void* arg)
 {
     char* str = (char*)arg;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 4; i++) {
         printf("hello from my thread ‘%s'\n", str);
         sleep(1);
     }
@@ -22,10 +22,19 @@ void* mythread(void* arg)
 int main() {
     mythread_t mytid;
     void* retval;
-    printf("main [%d %d %d]\n", getpid(), getppid(), gettid());
-    mythread_create(&mytid, mythread, "hello from main");
-    mythread_join(mytid, &retval);
-    printf("main [%d %d %d] thread returned '%s'\n", getpid(), getppid(), gettid(), (char*)retval);
+    int err;
 
+    printf("main [%d %d %d]\n", getpid(), getppid(), gettid());
+
+    int is_joinable = 0;
+
+    err = mythread_create(&mytid, is_joinable, mythread, "hello from main");
+    if (err) {
+        printf("Main thread: mythread_create() for Thread 3 failed\n");
+        return -1;
+    }
+    
+    // mythread_join(mytid, &retval);
+    printf("main [%d %d %d] thread returned '%s'\n", getpid(), getppid(), gettid(), (char*)retval);
     return 0;
 }
