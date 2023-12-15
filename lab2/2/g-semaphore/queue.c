242b15788c9bce4512463241c2ecefd6dc9e5ad7
Anton Nazarov <112681811+anton-ableton@users.noreply.github.com>
2023-12-15 22:45:24 +0700

    Update queue.c

--- lab2/2/g-semaphore/queue.c
+++ lab2/2/g-semaphore/queue.c
@@ -3,28 +3,37 @@
 #include "queue.h"
 #include "semaphore.h"
 
-sem_t semaphore;
+pthread_mutex_t mutex;
+sem_t items_available; 
+sem_t space_available; 
 
-
-void *qmonitor(void *arg) {
-    queue_t *q = (queue_t *) arg;
+void *qmonitor(void *arg)
+{
+    queue_t *q = (queue_t *)arg;
 
     printf("qmonitor: [%d %d %d]\n", getpid(), getppid(), gettid());
 
-    while (1) {
+    while (1)
+    {
         queue_print_stats(q);
         sleep(1);
     }
     return NULL;
 }
 
-queue_t *queue_init(int max_count) {
+queue_t *queue_init(int max_count)
+{
+
 
     int err;
-    sem_init(&semaphore, 0, 1);
+
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&items_available, 0, 0);
+    sem_init(&space_available, 0, max_count);
 
     queue_t *q = malloc(sizeof(queue_t));
-    if (!q) {
+    if (!q)
+    {
         printf("Cannot allocate memory for a queue\n");
         abort();
     }
@@ -38,7 +47,8 @@ queue_t *queue_init(int max_count) {
     q->add_count = q->get_count = 0;
 
     err = pthread_create(&q->qmonitor_tid, NULL, qmonitor, q);
-    if (err) {
+    if (err)
+    {
         printf("queue_init: pthread_create() failed: %s\n", strerror(err));
         abort();
     }
@@ -46,30 +56,55 @@ queue_t *queue_init(int max_count) {
     return q;
 }
 
-void queue_destroy(queue_t *q) {
-    for (int i = 0; i < q->count; ++i) {
-        qnode_t *tmp = q->first;
-        q->first = q->first->next;
-        free(tmp);
+void queue_destroy(queue_t *q)
+{
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&items_available);
+    sem_destroy(&space_available);
+
+    if (q == NULL)
+    {
+        return;
     }
+
+    pthread_cancel(q->qmonitor_tid);
+    pthread_join(q->qmonitor_tid, NULL);
+
+    qnode_t *current = q->first;
+    while (current != NULL)
+    {
+        qnode_t *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
     q->count = 0;
     q->first = NULL;
     q->last = NULL;
+
+    free(q);
 }
 
-int queue_add(queue_t *q, int val) {
-    sem_wait(&semaphore);
+int queue_add(queue_t *q, int val)
+{
+    sem_wait(&space_available);
+    pthread_mutex_lock(&mutex);
+
     q->add_attempts++;
 
     assert(q->count <= q->max_count);
 
-    if (q->count == q->max_count) {
-        sem_post(&semaphore);
+    if (q->count == q->max_count)
+    {
+        pthread_mutex_unlock(&mutex);
+        sem_post(&items_available);
         return 0;
     }
 
     qnode_t *new = malloc(sizeof(qnode_t));
-    if (!new) {
+    if (!new)
+    {
         printf("Cannot allocate memory for new node\n");
         abort();
     }
@@ -77,44 +112,50 @@ int queue_add(queue_t *q, int val) {
     new->val = val;
     new->next = NULL;
 
-
     if (!q->first)
         q->first = q->last = new;
-    else {
+    else
+    {
         q->last->next = new;
         q->last = q->last->next;
     }
     q->count++;
     q->add_count++;
-    sem_post(&semaphore);
+    pthread_mutex_unlock(&mutex);
+    sem_post(&items_available);
     return 1;
 }
 
-int queue_get(queue_t *q, int *val) {
-    sem_wait(&semaphore);
+int queue_get(queue_t *q, int *val)
+{
+    sem_wait(&items_available);
+    pthread_mutex_lock(&mutex);
     q->get_attempts++;
 
     assert(q->count >= 0);
 
-    if (q->count == 0) {
-        sem_post(&semaphore);
+    if (q->count == 0)
+    {
+        pthread_mutex_unlock(&mutex);
+        sem_post(&space_available);
         return 0;
     }
 
     qnode_t *tmp = q->first;
     *val = tmp->val;
 
-
     q->first = q->first->next;
 
     free(tmp);
     q->count--;
     q->get_count++;
-    sem_post(&semaphore);
+    pthread_mutex_unlock(&mutex);
+    sem_post(&space_available);
     return 1;
 }
 
-void queue_print_stats(queue_t *q) {
+void queue_print_stats(queue_t *q)
+{
     printf("queue stats: current size %d; attempts: (%ld %ld (dif:)%ld); counts (%ld %ld (dif:)%ld)\n",
            q->count,
            q->add_attempts, q->get_attempts, q->add_attempts - q->get_attempts,
